c1e653487337506455cc94f698b0c707e07c1d97
Thom Harty <thomharty@192.168.1.102>
2023-03-07 12:18:50 +0000

    Added List Bank Holidays Builder

---
+++ src/store/bankHolidays/listBankHolidays/listBankHolidays.builder.ts
@@ -0,0 +1,30 @@
+import {ActionReducerMapBuilder, PayloadAction} from '@reduxjs/toolkit';
+
+import {IBankHolidayState} from '../bankHolidays.types';
+import {listBankHolidays} from './listBankHolidays.thunk';
+import {IListBankHolidaysResponse} from './listBankHolidays.types';
+
+export const listBankHolidayBuilder = (
+  builder: ActionReducerMapBuilder<IBankHolidayState>,
+) => {
+  builder.addCase(listBankHolidays.pending, state => {
+    state.loading = true;
+  });
+  builder.addCase(listBankHolidays.rejected, state => {
+    state.loading = false;
+  });
+  builder.addCase(
+    listBankHolidays.fulfilled,
+    (state, action: PayloadAction<IListBankHolidaysResponse>) => {
+      state.loading = false;
+      const bankHolidays = [
+        ...action.payload['england-and-wales'].events,
+        ...action.payload.scotland.events,
+        ...action.payload['northern-ireland'].events,
+      ];
+      state.bankHolidays = bankHolidays;
+    },
+  );
+
+  return builder;
+};
