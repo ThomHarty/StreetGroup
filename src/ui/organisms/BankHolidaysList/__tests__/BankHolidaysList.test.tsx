32326eb517a7acb532415fdb17a21f2e3b57d64c
Thom Harty <thomharty@192.168.1.102>
2023-03-07 16:35:59 +0000

    Updated Bank Holiday Item Content

--- src/ui/organisms/BankHolidaysList/__tests__/BankHolidaysList.test.tsx
+++ src/ui/organisms/BankHolidaysList/__tests__/BankHolidaysList.test.tsx
@@ -7,20 +7,41 @@ import {IBankHolidaysListProps} from '../BankHolidaysList.types';
 describe('BankHolidaysList Organism', () => {
   const props: IBankHolidaysListProps = {
     list: [
-      {title: {text: 'One'}, date: {text: 'Today'}},
-      {title: {text: 'Two'}, date: {text: 'Tomorrow'}},
+      {
+        title: {text: 'One'},
+        date: {text: 'Today'},
+        notes: {text: 'First'},
+        bunting: false,
+      },
+      {
+        title: {text: 'Two'},
+        date: {text: 'Tomorrow'},
+        notes: {text: 'Second'},
+        bunting: true,
+      },
     ],
   };
 
   it('renders the title', () => {
     const {getByText} = render(<BankHolidaysList {...props} />);
-    expect(getByText('One')).toBeTruthy();
-    expect(getByText('Two')).toBeTruthy();
+    expect(getByText(props.list[0].title.text)).toBeTruthy();
+    expect(getByText(props.list[1].title.text)).toBeTruthy();
   });
 
   it('renders the date', () => {
     const {getByText} = render(<BankHolidaysList {...props} />);
-    expect(getByText('Today')).toBeTruthy();
-    expect(getByText('Tomorrow')).toBeTruthy();
+    expect(getByText(props.list[0].date.text)).toBeTruthy();
+    expect(getByText(props.list[1].date.text)).toBeTruthy();
+  });
+
+  it('renders the notes', () => {
+    const {getByText} = render(<BankHolidaysList {...props} />);
+    expect(getByText(props.list[0].notes.text)).toBeTruthy();
+    expect(getByText(props.list[1].notes.text)).toBeTruthy();
+  });
+
+  it("renders 'Bunting required' when bunting=true", () => {
+    const {getByText} = render(<BankHolidaysList {...props} />);
+    expect(getByText('Bunting required')).toBeTruthy();
   });
 });
--- src/ui/templates/BankHolidaysTemplate/__tests__/BankHolidaysTemplate.test.tsx
+++ src/ui/templates/BankHolidaysTemplate/__tests__/BankHolidaysTemplate.test.tsx
@@ -8,21 +8,42 @@ describe('BankHolidays Template', () => {
   const props: IBankHolidaysTemplateProps = {
     bankHolidays: {
       list: [
-        {title: {text: 'One'}, date: {text: 'Today'}},
-        {title: {text: 'Two'}, date: {text: 'Tomorrow'}},
+        {
+          title: {text: 'One'},
+          date: {text: 'Today'},
+          notes: {text: 'First'},
+          bunting: false,
+        },
+        {
+          title: {text: 'Two'},
+          date: {text: 'Tomorrow'},
+          notes: {text: 'Second'},
+          bunting: true,
+        },
       ],
     },
   };
 
   it('renders the title', () => {
     const {getByText} = render(<BankHolidaysTemplate {...props} />);
-    expect(getByText('One')).toBeTruthy();
-    expect(getByText('Two')).toBeTruthy();
+    expect(getByText(props.bankHolidays.list[0].title.text)).toBeTruthy();
+    expect(getByText(props.bankHolidays.list[1].title.text)).toBeTruthy();
   });
 
   it('renders the date', () => {
     const {getByText} = render(<BankHolidaysTemplate {...props} />);
-    expect(getByText('Today')).toBeTruthy();
-    expect(getByText('Tomorrow')).toBeTruthy();
+    expect(getByText(props.bankHolidays.list[0].title.text)).toBeTruthy();
+    expect(getByText(props.bankHolidays.list[1].title.text)).toBeTruthy();
+  });
+
+  it('renders the notes', () => {
+    const {getByText} = render(<BankHolidaysTemplate {...props} />);
+    expect(getByText(props.bankHolidays.list[0].notes.text)).toBeTruthy();
+    expect(getByText(props.bankHolidays.list[1].notes.text)).toBeTruthy();
+  });
+
+  it("renders 'Bunting required' when bunting=true", () => {
+    const {getByText} = render(<BankHolidaysTemplate {...props} />);
+    expect(getByText('Bunting required')).toBeTruthy();
   });
 });
