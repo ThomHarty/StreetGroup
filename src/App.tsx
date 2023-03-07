4418423ec26f19a6ce0a013bbfb7431e8914ad46
Thom Harty <thomharty@192.168.1.102>
2023-03-07 09:59:13 +0000

    Removed UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import React, {ReactElement, useState} from 'react';
-import {Keyboard, Pressable, SafeAreaView} from 'react-native';
-import {Button, Input, Typography} from './ui/atoms';
+import React, {ReactElement} from 'react';
+import {View} from 'react-native';
 
 export const App = (): ReactElement => {
-  const [value, setValue] = useState('');
-  return (
-    <SafeAreaView>
-      <Pressable onPress={() => Keyboard.dismiss()}>
-        <Typography text="title" />
-        <Input value={value} onChangeText={val => setValue(val)} />
-        <Button title="button" onPress={() => {}} />
-      </Pressable>
-    </SafeAreaView>
-  );
+  return <View />;
 };
